7df60d38915cbb3bdf25abc326ae4c4b128ae937
Sai Zin Phyo Lwin <sai@Sais-MacBook-Pro.local>
2025-12-28 13:27:59 -0800

    Update essays list

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -1,15 +1,24 @@
-export default function Essays() {
+import Link from "next/link";
+
+export default function Page() {
   return (
     <main className="page">
       <p>Here’s a list of my writing.</p>
 
       <ul>
         <li>
-          <a href="/essays/documentation-the-most-underrated-skill">
-            Documentation: The Most Underrated Skill
-          </a>
+          <Link href="/essays/ai-usage">AI Usage</Link>
+        </li>
+        <li>
+          <Link href="/essays/writing-is-thinking">Writing Is Thinking</Link>
+        </li>
+        <li>
+          <Link href="/essays/writing-to-communicate">
+            Writing to Communicate
+          </Link>
         </li>
       </ul>
     </main>
   );
 }
+
